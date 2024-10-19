2d11ea66b5ddbb3c6db8483c4026749ae1c1f591
Jerson-P <bolanosy8@gmail.com>
2024-10-18 20:22:56 -0500

    Primer commit - Proyecto de gestión de reservas

---
+++ src/main/java/com/prueba/bigview/gestionreservas/GestionreservasApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.bigview.gestionreservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionreservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionreservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/bigview/gestionreservas/GestionreservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.bigview.gestionreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionreservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
